ab86297cac1c8b91f3f70d8d1daa8a417273cec4
Wilkozx <116211882+Wilkozx@users.noreply.github.com>
2024-05-03 12:30:26 +0100

    added testing for staff class

---
+++ Skeleton/Testing1/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        { 
+            // create an instance of a Staff
+            clsStaff AStaff = new clsStaff();
+            // Check if it exists
+            Assert.IsNotNull(AStaff);
+        }
+    }
+}
