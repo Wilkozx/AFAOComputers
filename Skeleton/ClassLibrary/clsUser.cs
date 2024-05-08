4cc95239dc96d16b24d7532721890a48a48070b8
Omar Aldaboubi <P2687704@my365.dmu.ac.uk>
2024-05-08 09:59:33 +0100

    Created properties in TestingUser

--- Skeleton/ClassLibrary/clsUser.cs
+++ Skeleton/ClassLibrary/clsUser.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsUser
     {
+        public bool Active { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int PhoneNumber { get; set; }
     }
 }
