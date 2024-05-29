8ec93f9db5ae7d6e7ac17e1a11cd2d947d41da52
P2663364 <166540991+P2663364@users.noreply.github.com>
2024-05-29 09:18:26 +0100

    Order

---
+++ Skeleton/Testing1/tstOrder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
